5e2190b256e831678cb58709c3bdf61e3ac6bd65
Abhishek Goyal <Abhishekgoyal0149@gmail.com>
2026-04-07 10:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (81.16%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -5,18 +5,8 @@ public:
         if(n<=0) return false;
         if (n==1) return true;
         for(;n>1;)
-        {
-            if(n%2==0)
-            {
-                n=n/2;
-            }
-            else
-            {
-                return false;
-            }
-        }
+        {if(n%2==0) n=n/2;
+        else return false;}
         return true;
-        
-        
     }
 };
